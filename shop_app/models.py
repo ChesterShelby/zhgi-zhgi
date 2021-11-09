75f00ab5bb3d991e73eb152cb179bf50547f4087
Vadim <NazarovvorazaN@mail.ru>
2021-11-09 20:19:52 +0300

    base html

--- shop_app/models.py
+++ shop_app/models.py
@@ -1,7 +1,35 @@
 from django.db import models
 from django.contrib.auth import get_user_model
+from django.contrib.contenttypes.models import ContentType
+from django.contrib.contenttypes.fields import GenericForeignKey
+
 
 User = get_user_model()
+
+
+class LatestProductsManager:
+
+    @staticmethod
+    def get_products_for_main_page(*args, **kwargs):
+        with_respect_to = kwargs.get('with_respect_to')
+        products = []
+        ct_models = ContentType.objects.filter(model__in=args)
+        for ct_model in ct_models:
+            model_products = ct_model.model_class()._base_manager.all().order_by('-id')[:5]
+            products.extend(model_products)
+        if with_respect_to:
+            ct_model = ContentType.objects.filter(model=with_respect_to)
+            if ct_model.exists():
+                if with_respect_to in args:
+                    return sorted(
+                        products, key=lambda x: x.__class__._meta.model_name.startswith(with_respect_to), reverse=True
+                    )
+        return products
+
+
+class LatestProducts:
+
+    objects = LatestProductsManager()
 
 
 class Category(models.Model):
@@ -15,6 +43,9 @@ class Category(models.Model):
 
 class Product(models.Model):
 
+    class Meta:
+        abstract = True
+
     category = models.ForeignKey(Category, verbose_name='Категория', on_delete=models.CASCADE)
     title = models.CharField(max_length=255, verbose_name='Наименование')
     slug = models.SlugField(unique=True)
@@ -26,11 +57,32 @@ class Product(models.Model):
         return self.title
 
 
+class Tshirt(Product):
+
+    size = models.CharField(max_length=255, verbose_name='Размер')
+    color = models.CharField(max_length=255, verbose_name='Цвет')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
+class Pin(Product):
+
+    size = models.CharField(max_length=255, verbose_name='Размер')
+    thematics = models.CharField(max_length=255, verbose_name='Тематика')
+
+    def __str__(self):
+        return "{} : {}".format(self.category.name, self.title)
+
+
 class CartProduct(models.Model):
 
     user = models.ForeignKey('Customer', verbose_name='Покупатель', on_delete=models.CASCADE)
-    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
+    cart = models.ForeignKey('Cart', verbose_name='Корзина', on_delete=models.CASCADE, related_name='related_products')
+    content_type = models.ForeignKey(ContentType, on_delete=models.CASCADE)
+    object_id = models.PositiveIntegerField()
+    content_object = GenericForeignKey('content_type', 'object_id')
+    # product = models.ForeignKey(Product, verbose_name='Товар', on_delete=models.CASCADE)
     qty = models.PositiveIntegerField(default=1)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
@@ -41,7 +93,7 @@ class CartProduct(models.Model):
 class Cart(models.Model):
 
     owner = models.ForeignKey('Customer', verbose_name='Владелец', on_delete=models.CASCADE)
-    product = models.ManyToManyField(CartProduct, blank=True)
+    product = models.ManyToManyField(CartProduct, blank=True, related_name='related_cart')
     total_products = models.PositiveIntegerField(default=0)
     final_price = models.DecimalField(max_digits=9, decimal_places=2, verbose_name='Общая цена')
 
